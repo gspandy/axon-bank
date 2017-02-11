cab90f842d3bc8d5b786a0d9618c43987d416493
Richard O. Amoako Agyei <richard.agyei@thomsonreuters.com>
2017-02-10 22:17:54 -0600

    Webinar 2 Saga orchestration

--- src/main/java/com/richard/axon/AxonBankApplication.java
+++ src/main/java/com/richard/axon/AxonBankApplication.java
@@ -1,11 +1,8 @@
 package com.richard.axon;
 
-import org.axonframework.eventsourcing.eventstore.EventStore;
-import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 import org.axonframework.spring.config.EnableAxon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @EnableAxon //--spring configuration
 @SpringBootApplication
